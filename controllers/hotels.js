90bf2005d1b76c82fc51804a2a772c69dbe6975e
adnan4k <fayomuhe5@gmail.com>
2024-03-24 16:50:40 +0300

    count by type added

--- controllers/hotels.js
+++ controllers/hotels.js
@@ -1,59 +1,76 @@
-import Hotel  from "../models/Hotel.js";
+import Hotel from "../models/Hotel.js";
 
-export  const createHotel = async(req,res,next) =>{
-   const newHotel = new Hotel(req.body)
-    try {
-        const  savedHotel = await newHotel.save();
-        return res.status(201).json(savedHotel);
-    } catch (error) {
-        next(error)
-    }
-}
+export const createHotel = async (req, res, next) => {
+  const newHotel = new Hotel(req.body);
+  try {
+    const savedHotel = await newHotel.save();
+    return res.status(201).json(savedHotel);
+  } catch (error) {
+    next(error);
+  }
+};
 
-export const getHotel = async(req,res,next) =>{
-     
-    try {
-         const hotel = await Hotel.findById(req.params.id);
-         return res.status(200).json(hotel)
-    } catch (error) {
-        next(error)
-    }
-}
-export const getHotels = async(req,res,next) =>{
-     
-    try {
-         const hotels = await Hotel.find();
-         return res.status(200).json(hotels)
-    } catch (error) {
-        next(error)
-    }
-}
-export const countByCity = async(req,res,next) =>{
-      const cities = req.query.cities.split(",")
-    try {
-         const list = await Promise.all(cities.map(city =>{
-            return Hotel.countDocuments({city:city})
-         }));
-         return res.status(200).json(list)
-    } catch (error) {
-        next(error)
-    }
-}
-export const deleteHotel = async(req,res,next) =>{
-     
-    try {
-         const hotels = await Hotel.delete(req.params.id);
-         return res.status(200).json(hotels)
-    } catch (error) {
-        next(error)
-    }
-}
-export const updateHotel = async(req,res,next) =>{
-     
-    try {
-         const hotels = await Hotel.findByIdAndUpdate(req.params.id);
-         return res.status(200).json(hotels)
-    } catch (error) {
-        next(error)
-    }
-}
+export const getHotel = async (req, res, next) => {
+  try {
+    const hotel = await Hotel.findById(req.params.id);
+    return res.status(200).json(hotel);
+  } catch (error) {
+    next(error);
+  }
+};
+export const getHotels = async (req, res, next) => {
+  try {
+    const hotels = await Hotel.find();
+    return res.status(200).json(hotels);
+  } catch (error) {
+    next(error);
+  }
+};
+export const countByCity = async (req, res, next) => {
+  const cities = req.query.cities.split(",");
+  try {
+    const list = await Promise.all(
+      cities.map((city) => {
+        return Hotel.countDocuments({ city: city });
+      })
+    );
+    return res.status(200).json(list);
+  } catch (error) {
+    next(error);
+  }
+};
+export const countByType = async (req, res, next) => {
+  try {
+    const appartmentCount = await Hotel.countDocuments({ type: "hotel" });
+    const villaCount = await Hotel.countDocuments({ type: "villa" });
+    const resortCount = await Hotel.countDocuments({ type: "resort" });
+    const cabinCount = await Hotel.countDocuments({ type: "cabin" });
+    const hotelCount = await Hotel.countDocuments({ type: "hotel" });
+
+    return res
+      .status(200)
+      .json([{type:"appartments",count:appartmentCount}, 
+      {type:"villas",count:villaCount},
+       {type:"cabins",count:cabinCount},
+        {type:"hotels",count:hotelCount},
+         {type:"resorts",count:resortCount}]);
+  } catch (error) {
+    next(error);
+  }
+};
+export const deleteHotel = async (req, res, next) => {
+  try {
+    const hotels = await Hotel.delete(req.params.id);
+    return res.status(200).json(hotels);
+  } catch (error) {
+    next(error);
+  }
+};
+export const updateHotel = async (req, res, next) => {
+  try {
+    const hotels = await Hotel.findByIdAndUpdate(req.params.id);
+    return res.status(200).json(hotels);
+  } catch (error) {
+    next(error);
+  }
+};
--- routes/hotels.js
+++ routes/hotels.js
@@ -1,12 +1,12 @@
 import express, { Router } from "express"
-import { countByCity, createHotel, deleteHotel, getHotel, getHotels, updateHotel } from "../controllers/hotels.js"
+import { countByCity, countByType, createHotel, deleteHotel, getHotel, getHotels, updateHotel } from "../controllers/hotels.js"
 import { verifyAdmin } from "../utils/VerifyToken.js"
 
  const router = express.Router()
      router.post('/',verifyAdmin,createHotel)
      router.get('/find/:id',getHotel)
      router.get('/',getHotels)
-     router.get('/cityByType',getHotels)
+     router.get('/countByType',countByType)
      router.get('/countByCity',countByCity)
      router.delete('/:id',verifyAdmin,deleteHotel)
      router.put('/:id',verifyAdmin,updateHotel)
